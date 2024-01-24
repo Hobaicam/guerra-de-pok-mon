188da186183ad377ae0ecf9608027016484bf239
Hobaicam <156662790+Hobaicam@users.noreply.github.com>
2024-01-24 00:28:40 -0400

    Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -1,40 +1,43 @@
 import random
 
-jugador = [100, 100] # Jugador: [PS, defensa]
-oponente = [100, 0] # Oponente: [PS, defensa]
-
+jugador = [100, 100]  # Jugador: [PS, defensa]
+oponente = [100, 0]  # Oponente: [PS, defensa]
 
 # Ataques: [nombre, daño, efecto]
-ataques = [ ["malicioso", 0, 10], 
-           ["placaje", 35, 0],
-           ["ascuas", 20, 0]
+ataques = [
+    ["malicioso", 10],
+    ["placaje", 35],
+    ["ascuas", 20]
 ]
+
 while jugador[0] > 0 and oponente[0] > 0:
-    ataque_jugador = input ("ataque: ").lower()
+    # Turno del jugador
+    ataque_jugador = input("Elige un ataque (malicioso, placaje, ascuas): ").lower()
     ataque_encontrado = None
     for ataque in ataques:
         if ataque[0] == ataque_jugador:
             ataque_encontrado = ataque
             break
-
     if ataque_encontrado:
-        oponente[1] -= ataque_encontrado[2]
-        if oponente <= 0:
-            oponente[1]=1
-        oponente[0] -= ataque_encontrado[1] / (oponente[1]/100)
+        oponente[1] -= ataque_encontrado[1]
+        if oponente[1] <= 0:
+            oponente[1] = 1  # Defensa mínima
+        oponente[0] -= int(ataque_encontrado[1] / (oponente[1] / 100))
     else:
-        print("Cuidado, tus ataques son malicioso,placaje y ascuas")
+        print("Ataque no válido. Los ataques disponibles son: malicioso, placaje, ascuas")
 
-    #Turno del Oponente
-    ataque_oponente = random. choice (ataques)
-    if ataque_oponente[2] > 0:
-        jugador [1] -= ataque_oponente[2]
-        if jugador[1] <= 0:
-            jugador [1] = 1
+# Verificar si el oponente ha sido derrotado
+    if oponente[0] <= 0:
+        print("¡Felicidades, has ganado!")
+        break
+               
+# Turno del oponente
+    ataque_oponente = random.choice(ataques)
+    jugador[1] -= ataque_oponente[1]
+    if jugador[1] <= 0:
+        jugador[1] = 1  # Defensa mínima
+    jugador[0] -= int(ataque_oponente[1] / (jugador[1] / 100))
 
-    jugador [0] = ataque_oponente[1] * (100 / jugador[1])
-
-if oponente [0] <= 0:
-    print(" ¡Felicidades, has ganado!") 
-else:
-    print ("Lo siento, has perdido.")
+# Verificar si el jugador ha sido derrotado
+if jugador[0] <= 0:
+    print("Lo siento, has perdido.")
